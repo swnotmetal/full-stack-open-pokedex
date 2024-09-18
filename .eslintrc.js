efb499a26561e14573da6eda8a3614faf14d81a8
Shuang.W <66452432+swnotmetal@users.noreply.github.com>
2024-09-18 05:11:39 +0300

    11.6.3 attempt3

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
   "env": {
     "node": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
   },
   "extends": [
     "eslint:recommended",
@@ -16,7 +16,7 @@ module.exports = {
     "sourceType": "module"
   },
   "plugins": [
-    "react", "jest"
+    "react", "jest", "cypress"
   ],
   "rules": {
     "indent": [
